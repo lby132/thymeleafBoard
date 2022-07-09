f40da03a58abb9d386a7e4a0b5427c2b80565dd9
lby132 <quddbscjswow@naver.com>
2022-07-10 02:08:07 +0900

    글쓰기 페이지 생

--- Board/src/main/java/com/example/demo/controller/BoardController.java
+++ Board/src/main/java/com/example/demo/controller/BoardController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.domain.BoardDTO;
 import com.example.demo.service.BoardService;
 
+import lombok.RequiredArgsConstructor;
+
+@SuppressWarnings("unused")
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
 
 	@Autowired
@@ -26,4 +33,20 @@ public class BoardController {
 		
 		return "board/write";
 	}
+	
+	@GetMapping(value = "/board/write.do")
+	public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) {
+			model.addAttribute("board", new BoardDTO());
+		} else {
+			BoardDTO board = boardService.getBoardDetail(idx);
+			if (board == null) {
+				return "redirect:/board/list.do"; 
+			}
+			model.addAttribute("board", board);
+		}
+		return "board/write";
+	}
+	
+	
 }
