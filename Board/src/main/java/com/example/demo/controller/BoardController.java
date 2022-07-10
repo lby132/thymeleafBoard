1e57e3ca6498b7e2a0ad0c27838d9b656cf4618b
lby132 <quddbscjswow@naver.com>
2022-07-10 23:06:12 +0900

    등록 수정 삭제 완료

--- Board/src/main/java/com/example/demo/controller/BoardController.java
+++ Board/src/main/java/com/example/demo/controller/BoardController.java
@@ -1,39 +1,32 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequestInterceptor;
 
+import com.example.demo.constant.Method;
 import com.example.demo.domain.BoardDTO;
 import com.example.demo.service.BoardService;
+import com.example.demo.util.UiUtils;
 
 import lombok.RequiredArgsConstructor;
 
-@SuppressWarnings("unused")
+
 @Controller
 @RequiredArgsConstructor
-public class BoardController {
+public class BoardController extends UiUtils{
 
 	@Autowired
 	private BoardService boardService;
-	
-	@GetMapping(value = "/board/write.do")
-	public String openBoardWrite(Model model) {
-		
-		String title = "제목";
-		String content = "내용";
-		String writer = "홍길동";
-		
-		model.addAttribute("t", title);
-		model.addAttribute("c", content);
-		model.addAttribute("w", writer);
-		
-		return "board/write";
-	}
-	
+
+
 	@GetMapping(value = "/board/write.do")
 	public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
 		if (idx == null) {
@@ -48,5 +41,68 @@ public class BoardController {
 		return "board/write";
 	}
 	
+	@PostMapping(value = "/board/register.do")
+	public String registerBoard(final BoardDTO params, Model model) {
+		try {
+			boolean isRegistered = boardService.registerBoard(params);
+			if (isRegistered == false) {
+				return showMessageWithRedirect("게시글 등록에 실패햐였습니다.", "/board/list.do", Method.GET, null, model);
+			}
+		} catch (DataAccessException e) {
+			return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.", "/board/list.do", Method.GET, null, model);
+		} catch (Exception e) {
+			return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list.do", Method.GET, null, model);
+		}
+		
+		return showMessageWithRedirect("게시글 등록이 완료되었습니다.", "/board/list.do", Method.GET, null, model);
+	}
+	
+	@GetMapping(value = "/board/list.do")
+	public String openBoardList(Model model) {
+		List<BoardDTO> boardList = boardService.getBoardList();
+		model.addAttribute("boardList", boardList);
+		
+		return "board/list";
+	}
+	
+	@GetMapping(value = "/board/view.do")
+	public String openBoardDetail(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) {
+			model.addAttribute("resultMsg", "올바르지 않은 접근입니다.");
+			return "redirect:/board/list.do";
+		} 
+		
+		BoardDTO board = boardService.getBoardDetail(idx);
+		if (board == null || "Y".equals(board.getDeleteYn())) {
+			model.addAttribute("result", "없는 게시물이거나 이미 삭제된 게시물 입니다.");
+			return "redirect:/board/list.do";
+		}
+		
+		model.addAttribute("board", board);
+		
+		return "board/view";
+	}
+	
+	@PostMapping(value = "/board/delete.do")
+	public String deleteBoard(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) {
+			return showMessageWithRedirect("올바르지 않은 접근입니다.", "/board/list.do", Method.GET, null, model);
+		}
+		
+		try {
+			boolean isDeleted = boardService.deleteBoard(idx);
+			if (isDeleted == false) {
+				return showMessageWithRedirect("게시글 삭제에 실패하였습니다.", "/board/list.do", Method.GET, null, model);
+			}
+		} catch (DataAccessException e) {
+			return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.", "/board/list.do", Method.GET, null, model);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list.do", Method.GET, null, model);
+		}
+		return showMessageWithRedirect("게시글 삭제가 완료되었습니다.", "/board/list.do", Method.GET, null, model);
+	}
+	
+
 	
 }
