c7ab3d3a83d02ed00026718a7bac094b78ddd642
lby132 <quddbscjswow@naver.com>
2022-07-23 01:26:39 +0900

    댓글 기능 추가중

--- Board/src/main/java/com/example/demo/domain/BoardDTO.java
+++ Board/src/main/java/com/example/demo/domain/BoardDTO.java
@@ -1,12 +1,6 @@
 package com.example.demo.domain;
 
-import java.time.LocalDateTime;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class BoardDTO extends CommonDTO{
 
 	/** 번호 (PK) */
